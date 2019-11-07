b0f9df48a00675fb3b00f4912e45c154c59c44c0
Mangena GiriNagaPavanKumar <M.GiriNagaPavanKumar@Ltts.com>
2019-11-07 16:59:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+const API_URL = 'http://jsonplaceholder.typicode.com';
+class App extends React.Component {
+  state = {
+    values  : []
+  }
+  componentDidMount() {
+    const url = `${API_URL}/users/`;
+    axios.get(url).then(res => res.data)
+    .then((data) => {
+      this.setState({ values  : data })
+      console.log(this.state.values)
+     })
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>React Axios Example</h1>
+          {this.state.values.map((keys) => (
+            <div>
+              <div>
+                <h3>{keys.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
